d84f0273872b5e0ff9e7d32a1a3d36cb6c7ff10e
dodong471520 <1012131075@qq.com>
2018-03-12 00:56:28 +0800

    ds

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+rm ~/.vim
 ln -s ~/.ghq/github.com/dodong471520/.vim ~/.vim
+rm ~/.vimrc
 ln -s ~/.ghq/github.com/dodong471520/space-vim/init.vim ~/.vimrc
+rm ~/.space-vim
+ln -s ~/.ghq/github.com/dodong471520/space-vim ~/.space-vim
+rm ~/.spacevim
+ln -s ~/.ghq/github.com/dodong471520/space-vim/init.spacevim ~/.spacevim
