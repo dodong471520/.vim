9760bcf753c46f026a79eafa87861efdcc2254b5
dodong471520 <1012131075@qq.com>
2018-03-25 23:38:00 +0800

    ds

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIG_PATH=~/.ghq/github.com/dodong471520
-ln -fs $CONFIG_PATH/.vim ~/.vim
-ln -fs $CONFIG_PATH/space-vim/init.vim ~/.vimrc
-ln -fs $CONFIG_PATH/space-vim ~/.space-vim
-ln -fs $CONFIG_PATH/space-vim/init.spacevim ~/.spacevim
